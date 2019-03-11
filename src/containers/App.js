23e373ae83953be37cfc4a4bd6d42c1a746ffbc3
github username <github email address>
2019-03-10 21:18:59 -0400

    navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Carousel from '../components/Carousel'
+import NavBar from '../components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar/>
       Restaurant Roulette
       <Carousel/>
       </div>
